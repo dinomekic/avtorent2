979f7075845005c352d2d921e3bf50fd0c9de56f
dinomekic <dino.mekic@gmail.com>
2026-05-18 14:47:51 +0200

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,29 +5,24 @@ export function middleware(req: NextRequest) {
 
   if (pathname.startsWith('/admin') && pathname !== '/admin/login') {
     const token = req.cookies.get('avtorent-admin-token')?.value
-    if (!token) return NextResponse.redirect(new URL('/admin/login', req.url))
+    if (!token) return NextResponse.redirect(new URL('/login-kartica', req.url))
   }
-
   if (pathname.startsWith('/partner') && pathname !== '/partner/login') {
     const token = req.cookies.get('avtorent-partner-token')?.value
     if (!token) return NextResponse.redirect(new URL('/partner/login', req.url))
   }
-
   if (pathname.startsWith('/pranje') && pathname !== '/pranje/login') {
     const token = req.cookies.get('avtorent-wash-token')?.value
     if (!token) return NextResponse.redirect(new URL('/pranje/login', req.url))
   }
-
   if (pathname.startsWith('/saradnik') && pathname !== '/saradnik/login') {
     const token = req.cookies.get('avtorent-collab-token')?.value
     if (!token) return NextResponse.redirect(new URL('/saradnik/login', req.url))
   }
-
   if (pathname.startsWith('/moje') && pathname !== '/moje/login') {
     const token = req.cookies.get('avtorent-client-token')?.value
     if (!token) return NextResponse.redirect(new URL('/moje/login', req.url))
   }
-
   return NextResponse.next()
 }
 
